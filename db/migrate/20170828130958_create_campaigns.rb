50af941abb41a922bb98eadc038609682ff692b4
mak1323 <myleskaeding@gmail.com>
2017-08-28 09:12:26 -0400

    Created the initi db
    Initial db is campaigns that belong to users. Campaign just have a
    status of name. This will later become a one to many relationship with
    logs.

---
+++ db/migrate/20170828130958_create_campaigns.rb
@@ -0,0 +1,11 @@
+class CreateCampaigns < ActiveRecord::Migration[5.0]
+  def change
+    create_table :campaigns do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
